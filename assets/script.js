93ca0b6967375855d6f6f1c313174f1150fd700d
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-07-17 08:45:05 +0300

    pushing first function

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,63 @@ function numbersOnly(input)
   input.value=input.value.replace(regex, "");
 
 
-  const inputA = document.getElementById('shadeA');
-  const btn = document.getElementById('button1');
-  const outputA = document.getElementById('output0');
+  var inputA = document.getElementById('shadeA');
+  var btn = document.getElementById('button1');
+  var outputA = document.getElementById('output0');
 }
+
+var inputA = document.getElementById('input0');
+// var bttn = document.getElementById('button1');
+var outputA = document.getElementById('output0');
+
+var inputB = document.getElementById('input1');
+var outputB = document.getElementById('output1');
+
+var inputC = document.getElementById('input2')
+var outputC = document.getElementById('output2');
+
+var inputD = document.getElementById('input3');
+var outputD = document.getElementById('output3')
+
+var bttn = document.getElementById('button1');
+
+// var inputB = dicument.getElementById('input1');
+// var bttn = document.getElementById('button1');
+// var outputB = document.getElementById('output1');
+
+function fun1 (){
+  output0.innerHTML = inputA.value;
+  output1.innerHTML = inputB.value;
+  output2.innerHTML = inputC.value;
+  output3.innerHTML = inputD.value;
+}
+button1.addEventListener ('click', fun1);
+
+
+// const inputB = document.getElementById('input1');
+// const bttn = document.getElementById('button1');
+// const outputB = document.getElementById('output1');
+
+// function fun1 (){
+//   output0.innerHTML = inputA.value;
+// }
+// button1.addEventListener ('click', fun1);
+
+// const inputA = document.getElementById('input0');
+// const bttn = document.getElementById('button1');
+// const outputA = document.getElementById('output0');
+
+// function fun1 (){
+//   output0.innerHTML = inputA.value;
+// }
+// button1.addEventListener ('click', fun1);
+
+
+// const inputA = document.getElementById('input0');
+// const bttn = document.getElementById('button1');
+// const outputA = document.getElementById('output0');
+
+// function fun1 (){
+//   output0.innerHTML = inputA.value;
+// }
+// button1.addEventListener ('click', fun1);
